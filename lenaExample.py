a6bd4add7c6877613b31cceafc4f935a768138ec
benjamin.nie.ubc@gmail.com <benjamin.nie.ubc@gmail.com>
2014-10-25 01:40:17 -0700

    finishing up CoordinateInFishzone class

--- lenaExample.py
+++ lenaExample.py
@@ -1,7 +1,15 @@
 from scipy import misc
+import matplotlib.pyplot as plt
 
 # loads an 8bit grayscale image of Lena (classic image processing example) into l
 l = misc.lena()
 
 # saves image of lena into a png file
 misc.imsave('lena.png', l)
+
+# displays the image of lena
+plt.imshow(l)
+plt.show()
+
+# creates numpy array from an image file
+lena = misc.imread('lena.png')
